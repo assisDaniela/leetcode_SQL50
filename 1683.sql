4937956dd64ec67476c8c9148b6c3f859e2d7ba6
assisDaniela <147951948+assisDaniela@users.noreply.github.com>
2023-11-16 09:14:38 -0300

    Adicionado soluções para tópico Select

---
+++ 1683.sql
@@ -0,0 +1,10 @@
+-- SQL Schema Example
+Create table If Not Exists Tweets(tweet_id int, content varchar(50));
+Truncate table Tweets;
+insert into Tweets (tweet_id, content) values ('1', 'Vote for Biden');
+insert into Tweets (tweet_id, content) values ('2', 'Let us make America great again!');
+
+-- Solution
+SELECT tweet_id
+FROM Tweets
+WHERE length(content) > 15;
